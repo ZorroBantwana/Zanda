6a8a047fdd1fd32b529d8c52257ea088ee4426a4
ZorroBantwana <48716504+ZorroBantwana@users.noreply.github.com>
2019-03-22 12:04:31 +0200

    Update recursion.py

--- Zanda/recursion.py
+++ Zanda/recursion.py
@@ -1,24 +1,18 @@
 import numpy as np
 def sum_array(array):
-    sum = 0
+    sum_1 = 0
     for item in array:
-        sum+= item
-    return sum
+        sum_1 += item
+    return sum_1
 
 def fibonacci(n):
     if n <= 1:
         return 1
     return fibonacci(n - 2) + fibonacci(n - 1)
 
-    def factorial(n):
+def factorial(n):
     if n == 1:
         return n
-    else:
-        return n * factorial(n-1)
-
-def factorial(n):
-    if n == 0:
-        return 1
     else:
         return n * factorial(n-1)
 
